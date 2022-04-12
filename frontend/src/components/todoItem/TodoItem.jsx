72e6b2b95f57aab54183f12b425f63e8f56ad434
Eduardo Caparros <educaparrosbarcelo@gmail.com>
2022-04-12 20:02:02 +0200

    Simple todo item style

--- frontend/src/components/todoItem/TodoItem.jsx
+++ frontend/src/components/todoItem/TodoItem.jsx
@@ -1,5 +1,13 @@
+import './todoItem.css'
+
 const TodoItem = ({ name, description }) => {
-	return <div>{name}</div>
+	return (
+		<div className='todo_item'>
+			<h3 className='todo_item_name'>{name}</h3>
+			<h4 className='todo_item_description'>{description}</h4>
+			<span className='todo_item_delete'>x</span>
+		</div>
+	)
 }
 
 export default TodoItem
